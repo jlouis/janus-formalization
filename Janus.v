b8b5178f92d44d6358f215d9b058d868b98246cc
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2008-11-21 16:00:05 +0100

    Implement full JANUS syntax sans arrays and f(u)ncalls.

--- Janus.v
+++ Janus.v
@@ -1,4 +1,11 @@
-(* Formalisation of the JANUS language itself *)
+(* Formalisation of the JANUS language itself
+   The Operational semantics is taken from
+
+   Gl\"uck & Yokoyama - A Reversible Programming Language
+     and its Invertible Self-Interpreter, PEPM 2007, Nice,France,
+     ACM (cited as {PEPM2007})
+
+*)
 
 (* Current implementation is highly partial *)
 
@@ -16,21 +23,60 @@ Require Import MemMonad.
 Section Janus.
   Open Scope Z_scope.
 
+  (* Janus Expressions. These are taken from {PEPM2007}, Figure 1 *)
+  (* TODO: Arrays *)
   Inductive Exp : Set :=
   | E_Const : Z -> Exp
   | E_Var   : nat -> Exp
+
+  (* Arithmetic *)
   | E_Plus  : Exp -> Exp -> Exp
   | E_Minus : Exp -> Exp -> Exp
   | E_Mul   : Exp -> Exp -> Exp
+  | E_Div   : Exp -> Exp -> Exp
+  | E_Mod   : Exp -> Exp -> Exp
+
+  (* Bitwise operations *)
+  | E_Bit_Xor : Exp -> Exp -> Exp
+  | E_Bit_And : Exp -> Exp -> Exp
+  | E_Bit_Or : Exp -> Exp -> Exp
+
+  (* Comparisons *)
   | E_Eq    : Exp -> Exp -> Exp
   | E_Neq   : Exp -> Exp -> Exp
   | E_And   : Exp -> Exp -> Exp
-  | E_Or    : Exp -> Exp -> Exp.
+  | E_Or    : Exp -> Exp -> Exp
+  | E_Lt    : Exp -> Exp -> Exp
+  | E_Gt    : Exp -> Exp -> Exp
+  | E_Leq   : Exp -> Exp -> Exp
+  | E_Geq   : Exp -> Exp -> Exp.
+
+  (* Statements in the JANUS language.
+     TODO: Arrays, Funcalls *)
+  Inductive Stmt : Set :=
+  (* Skewed operations *)
+  | S_Incr : var -> Exp -> Stmt
+  | S_Decr : var -> Exp -> Stmt
+  | S_Xor  : var -> Exp -> Stmt
+
+  (* Misc *)
+  | S_Swap : var -> var -> Stmt
+  | S_Skip : Stmt
+
+  (* Control flow *)
+  | S_If : Exp -> Stmt -> Stmt -> Exp -> Stmt
+  | S_Loop : Exp -> Stmt -> Stmt -> Exp -> Stmt
+
+  (* Catenation *)
+  | S_Semi : Stmt -> Stmt -> Stmt.
+
+  (* What does an expression denote? Dynamic semantics of evaluating
+     expressions in JANUS *)
 
   Fixpoint denoteExp (m : memory) (e : Exp) {struct e} : Z :=
-    match e with
+    match e with 
       | E_Const z => z
-      | E_Var l => m l
+      | E_Var l => m l 
       | E_Plus e1 e2 => denoteExp m e1 + denoteExp m e2
       | E_Minus e1 e2 => denoteExp m e1 - denoteExp m e2
       | E_Mul e1 e2 => denoteExp m e1 * denoteExp m e2
@@ -52,22 +98,13 @@ Section Janus.
         match (denoteExp m e1, denoteExp m e2) with
           | (1,_) => 1
           | (_,1) => 1
-          | (_,_) => 0
-        end
+          | (_,_) => 0 end
     end.
 
   Theorem Exp_fwd_det : forall (m : memory) (e : Exp) v1 v2,
     (denoteExp m e = v1) /\ (denoteExp m e = v2) -> (v1 = v2).
     induction e; intuition.
   Qed.
-
-  Inductive Stmt : Set :=
-  | S_Incr : var -> Exp -> Stmt
-  | S_Decr : var -> Exp -> Stmt
-  | S_Swap : var -> var -> Stmt
-  | S_If : Exp -> Stmt -> Stmt -> Exp -> Stmt
-  | S_Semi : Stmt -> Stmt -> Stmt.
-
 
   Fixpoint denoteStmt (s : Stmt) : memM unit :=
     match s with
