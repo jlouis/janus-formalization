1d4e51e3fc10b7aea56cc0fac997d917a3637024
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-10 23:53:37 +0200

    Prove everything for xor as well.

--- Janus1.v
+++ Janus1.v
@@ -143,6 +143,7 @@ Section Janus1.
     | S_Skip : Stm
     | S_Incr : Var -> Exp -> Stm
     | S_Decr : Var -> Exp -> Stm
+    | S_Xor  : Var -> Exp -> Stm
     | S_Semi : Stm -> Stm -> Stm
     | S_If : Exp -> Stm -> Stm -> Exp -> Stm
     | S_Call : Z -> Stm
@@ -165,6 +166,12 @@ Section Janus1.
       r = (Word32.sub z' z) ->
       m' = W32Mem.write m v r ->
       Stm_eval d m (S_Decr v e) m'
+    | se_assvar_xor: forall (d : defs) (m m': mem) (v: Var) (n n' n'': w32) (e: Exp),
+      denote_Exp (W32Mem.hide m v) e = Some n ->
+      m v = Some n' ->
+      Word32.xor n n' = n'' ->
+      m' = W32Mem.write m v n'' ->
+      Stm_eval d m (S_Xor v e) m'
     | se_semi: forall (d : defs) (m m' m'': mem) (s1 s2: Stm),
       Stm_eval d m s1 m' ->
       Stm_eval d m' s2 m'' ->
@@ -221,12 +228,12 @@ Section Janus1.
     Proof.
       intros. unfold stm_equiv. grind.
       inversion H. inversion H4. subst.
-        assert (Stm_eval d m'1 (S_Semi s2 s3) m'). constructor 4 with (m' := m'0);
-          assumption. constructor 4 with (m' := m'1); assumption.
+        assert (Stm_eval d m'1 (S_Semi s2 s3) m'). constructor 5 with (m' := m'0);
+          assumption. constructor 5 with (m' := m'1); assumption.
       inversion H. subst. inversion H6. subst.
-        assert (Stm_eval d m (S_Semi s1 s2) m'1). constructor 4 with (m' := m'0);
+        assert (Stm_eval d m (S_Semi s1 s2) m'1). constructor 5 with (m' := m'0);
           assumption.
-        constructor 4 with (m' := m'1); assumption.
+        constructor 5 with (m' := m'1); assumption.
     Qed.
 
     Definition fwd_det G m s m' :=
@@ -248,6 +255,10 @@ Section Janus1.
       inversion H4. subst.
       assert (z' = z'0). grind.
       assert (z = z0). grind. grind.
+
+      inversion H4. subst.
+      assert (n = n0). grind.
+      assert (n' = n'0). grind. grind.
 
       inversion H0. subst. apply IHStm_eval2. assert (m' = m'0). eapply IHStm_eval1. eauto.
       subst. trivial.
@@ -283,6 +294,15 @@ Section Janus1.
       assert (z' = z'0). subst. eapply Word32.sub_eq_l. eauto.
       subst. apply (W32Mem.hide_eq m m'' v z'0). trivial. trivial. trivial.
 
+      inversion H4. subst.
+      assert (W32Mem.hide m v = W32Mem.hide m'' v). eapply W32Mem.write_hide. eauto.
+      assert (Word32.xor n n' = Word32.xor n0 n'0). assert (W32Mem.write m v (Word32.xor n n') v = W32Mem.write m'' v (Word32.xor n0 n'0) v).
+      apply equal_f. trivial. eapply W32Mem.write_eq_2. eauto.
+      assert (n = n0). assert (Some n = Some n0). rewrite H2 in H0. congruence. grind.
+      subst.
+      assert (n'0 = n'). eapply Word32.xor_mine. eauto.
+      subst. eapply (W32Mem.hide_eq m m'' v); eauto.
+
       inversion H0. subst. assert (m' = m'0). apply IHStm_eval2. trivial.
       subst. apply IHStm_eval1. trivial.
 
@@ -307,6 +327,7 @@ Section Janus1.
         | S_Skip => S_Skip
         | S_Incr x e => S_Decr x e
         | S_Decr x e => S_Incr x e
+        | S_Xor x e => S_Xor x e
         | S_Semi s1 s2 => S_Semi (invert s2) (invert s1)
         | S_If e1 s1 s2 e2 => S_If e2 (invert s1) (invert s2) e1
         | S_Call d => S_Uncall d
@@ -349,6 +370,17 @@ Section Janus1.
             rewrite Word32.add_zero. trivial.
         rewrite H3. trivial.
         rewrite W32Mem.hide_write. rewrite H2. rewrite W32Mem.hide_write. trivial.
+      simpl. apply (se_assvar_xor d m' m v n n'' n').
+        rewrite H2. rewrite W32Mem.hide_write. assumption.
+        rewrite H2. apply W32Mem.write_eq.
+        assert (n' = Word32.xor n n'').
+        rewrite <- H1. rewrite <- Word32.xor_assoc. rewrite Word32.xor_x_x_zero.
+        rewrite Word32.xor_commut. rewrite Word32.xor_zero. trivial.
+        symmetry. trivial.
+      apply (W32Mem.hide_eq m (W32Mem.write m' v n') v n'). trivial.
+        rewrite W32Mem.write_eq. trivial. rewrite W32Mem.hide_write. rewrite H2.
+        rewrite W32Mem.hide_write. trivial.
+
       simpl. eapply se_semi. eauto. trivial.
       simpl. eapply se_if_t; eauto.
       simpl. eapply se_if_f; eauto.
@@ -384,6 +416,17 @@ Section Janus1.
         rewrite H9. apply W32Mem.write_eq.
         rewrite W32Mem.hide_write. rewrite H8. rewrite W32Mem.hide_write.
         trivial.
+      intros.
+        simpl in H. inversion H.
+      apply (se_assvar_xor G m0 m'0 v n n'' n').
+        rewrite H8. rewrite W32Mem.hide_write. assumption.
+        rewrite H8. apply W32Mem.write_eq.
+        assert (n' = Word32.xor n n'').
+        rewrite <- H6. rewrite <- Word32.xor_assoc. rewrite Word32.xor_x_x_zero.
+        rewrite Word32.xor_commut. rewrite Word32.xor_zero. trivial.
+        symmetry. trivial. 
+        apply (W32Mem.hide_eq m'0 (W32Mem.write m0 v n') v n'). trivial. apply W32Mem.write_eq.
+        rewrite W32Mem.hide_write. rewrite H8. rewrite W32Mem.hide_write. trivial.
       intros.
       inversion H. apply (se_semi G m0 m'1 m'0). apply IHs1. trivial.
         apply IHs2. trivial.
