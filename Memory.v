e3028582ed0147db921a921e30e8e4ae476b7245
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2009-05-14 22:18:30 +0200

    Make the library more streamlined. Removes overfull h-boxes.

--- Memory.v
+++ Memory.v
@@ -10,7 +10,8 @@ Module Type STORE.
 
   (* locations have equality *)
   Parameter eq : location -> location -> Prop.
-  Parameter location_eq_dec : forall (n m : location), {n = m} + {n <> m}.
+  Parameter location_eq_dec : forall (n m : location),
+    {n = m} + {n <> m}.
 
 End STORE.
 
@@ -47,8 +48,10 @@ Module Mem(V : STORE).
     write m a v1 a = write m' a v2 a -> v1 = v2.
   Proof.
     intros. assert (Some v1 = Some v2).
-    assert (write m a v1 a = Some v1). apply write_eq.
-    assert (write m' a v2 a = Some v2). apply write_eq.
+    assert (write m a v1 a = Some v1).
+    apply write_eq.
+    assert (write m' a v2 a = Some v2).
+    apply write_eq.
     grind. injection H0. grind.
   Qed.
 
@@ -65,21 +68,29 @@ Module Mem(V : STORE).
   Lemma hide_write: forall m x v,
     hide (write m x v) x = hide m x.
   Proof.
-    intros. apply extensionality. intro. unfold hide, write. destruct (V.location_eq_dec x x0).
-    trivial. trivial.
+    intros.
+    apply extensionality. intro.
+    unfold hide, write.
+    destruct (V.location_eq_dec x x0).
+    trivial.
+    trivial.
   Qed.
 
   Lemma hide_write_2 : forall m m' x x' v1 v2 ,
     m = m' ->
       hide (write m x v1) x x' = hide (write m' x v2) x x'.
   Proof.
-    intros. unfold hide, write. destruct (V.location_eq_dec x x'); grind.
+    intros.
+    unfold hide, write.
+    destruct (V.location_eq_dec x x'); grind.
   Qed.
 
   Lemma hide_retract : forall m m' x,
     m = m' -> hide m x = hide m' x.
   Proof.
-    intros. apply extensionality. intro. unfold hide.
+    intros.
+    apply extensionality. intro.
+    unfold hide.
     destruct (V.location_eq_dec x x0); grind.
   Qed.
 
@@ -95,7 +106,9 @@ Module Mem(V : STORE).
   Lemma hide_ne: forall m x x',
     x <> x' -> hide m x x' = m x'.
   Proof.
-    intros. unfold hide. destruct (V.location_eq_dec x x').
+    intros.
+    unfold hide.
+    destruct (V.location_eq_dec x x').
     absurd (x = x'); assumption.
     trivial.
   Qed.
@@ -103,12 +116,20 @@ Module Mem(V : STORE).
   Lemma hide_irrel : forall m m' x,
     hide m x = hide m' x -> forall a, a <> x -> m a = m' a.
   Proof.
-    intros. assert (hide m x a = hide m' x a).
-    apply (equal_f V.location (option value) (hide m x) (hide m' x)). assumption.
-    assert (hide m x a = m a). unfold hide. destruct (V.location_eq_dec x a).
+    intros.
+    assert (hide m x a = hide m' x a).
+      apply
+        (equal_f V.location
+          (option value) (hide m x) (hide m' x)).
+      assumption.
+    assert (hide m x a = m a).
+      unfold hide.
+      destruct (V.location_eq_dec x a).
     symmetry in e. contradiction. trivial.
-    assert (hide m' x a = m' a). unfold hide. destruct (V.location_eq_dec x a).
-    symmetry in e. contradiction. trivial.
+    assert (hide m' x a = m' a).
+      unfold hide.
+      destruct (V.location_eq_dec x a).
+      symmetry in e. contradiction. trivial.
     grind.
   Qed.
   Hint Rewrite hide_irrel: mortar.
@@ -116,7 +137,10 @@ Module Mem(V : STORE).
   Lemma write_neutral : forall m x v,
     m x = Some v -> write m x v = m.
   Proof.
-    intros. apply extensionality. intro. unfold write.
+    intros.
+    apply extensionality.
+    intro.
+    unfold write.
     destruct (V.location_eq_dec x x0); grind.
   Qed.
 
@@ -126,12 +150,25 @@ Module Mem(V : STORE).
     m x = Some v -> m' x = Some v -> hide m x = hide m' x -> m = m'.
   Proof.
     intros.
-    assert (forall a, a <> x -> m a = m' a). apply hide_irrel. assumption.
-    assert (m = write m x v). symmetry. apply write_neutral; grind.
-    assert (m' = write m' x v). symmetry. apply write_neutral; grind.
-    rewrite H3. rewrite H4. apply extensionality. intro. unfold write.
-    destruct (V.location_eq_dec x x0). trivial. apply H2. apply sym_not_eq.
+    assert (forall a, a <> x -> m a = m' a).
+      apply hide_irrel.
       assumption.
+    assert (m = write m x v).
+      symmetry.
+      apply write_neutral; grind.
+    assert (m' = write m' x v).
+      symmetry.
+      apply write_neutral; grind.
+    rewrite H3.
+    rewrite H4.
+    apply extensionality.
+    intro.
+    unfold write.
+    destruct (V.location_eq_dec x x0).
+    trivial.
+    apply H2.
+    apply sym_not_eq.
+    assumption.
   Qed.
   Hint Rewrite hide_eq: mortar.
 
@@ -140,9 +177,14 @@ Module Mem(V : STORE).
   Proof.
     intros.
     apply extensionality. intro.
-    unfold hide. destruct (V.location_eq_dec x x0). trivial.
-    assert ((write m x v1 x0) = m x0). apply write_ne. trivial.
-    assert ((write m' x v2 x0) = m' x0). apply write_ne. trivial.
+    unfold hide.
+    destruct (V.location_eq_dec x x0). trivial.
+    assert ((write m x v1 x0) = m x0).
+      apply write_ne.
+      trivial.
+    assert ((write m' x v2 x0) = m' x0).
+      apply write_ne.
+      trivial.
     grind.
   Qed.
 
